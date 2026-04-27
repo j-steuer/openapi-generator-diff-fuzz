9b0646ea885b6808df205c74a4474335fd075479
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-04-27 14:07:58 +0200

    feat: start implementing Operation Id based ClientLibraryContainer, implement method inference

--- src/telephuzz/session/client_library.py
+++ src/telephuzz/session/client_library.py
@@ -6,6 +6,7 @@ from typing import Callable
 from docker.models.containers import Container
 
 from telephuzz.http_message import Request, Response
+from telephuzz.operation_ids import Case
 
 LibraryId = str
 Translation = Callable | list[str]
@@ -29,3 +30,26 @@ class ClientLibraryContainer(ABC):
     def send(self, request: Request) -> Response:
         """Send a request through the client library."""
         raise NotImplementedError  # TODO
+
+
+class OperationIdBasedCLC(ClientLibraryContainer):
+    """General-purpose client library container.
+
+    Can be used if the client-generation tool reliably uses the operation id
+    of a method/path pair to generate method names.
+    """
+
+    def __init__(self, id: LibraryId, container: Container, case: Case):
+        """Initialize the client library container."""
+        self.id = id
+        self.container = container
+        self.case = case
+
+    def _translate(self, request: Request) -> Translation:
+        # method, path = request.method, request.path
+        # operation_id = generate_operation_id(str(method), path)
+        # library_method_name = transform_case(operation_id, self.case)
+
+        # TODO create Translation based on library method name
+
+        raise NotImplementedError
