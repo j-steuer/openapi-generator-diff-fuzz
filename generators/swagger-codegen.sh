1a3259c414752a4f9338870d8fabbdd2c4fdcb5d
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-05-19 16:14:16 +0200

    fix: fix openapi-generator.sh output structure for C#

--- generators/swagger-codegen.sh
+++ generators/swagger-codegen.sh
@@ -9,4 +9,24 @@ for LANG in "${LANGS[@]}"; do
 
   docker run --rm -v $(pwd):/local swaggerapi/swagger-codegen-cli-v3:3.0.80 generate -i /local/spec/openapi.json -l "$LANG" -o /local/clients/swagger-codegen-$LANG-client
 
+
+  # --- C# post-processing: flatten src/IO.Swagger ---
+  if [ "$LANG" = "csharp" ]; then
+    echo "Post-processing C# output structure..."
+
+    OUT_DIR="clients/swagger-codegen-csharp-client"
+    TMP_DIR="clients/swagger-codegen-csharp-client_temp"
+    SRC_DIR="clients/swagger-codegen-csharp-client/src/IO.Swagger"
+    
+  if [ -e "$SRC_DIR" ]; then
+      echo "Moving contents of $SRC_DIR to $OUT_DIR"
+
+      mv "$SRC_DIR" "$TMP_DIR"
+      rm -r "$OUT_DIR"
+      mv "$TMP_DIR" "$OUT_DIR"
+
+  else
+      echo "File $SRC_DIR does not exist"
+  fi
+fi
 done
