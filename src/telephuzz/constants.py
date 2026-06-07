fbcec8a937c4d87252461f0adf93719c5f5b944c
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-06-07 15:35:07 +0200

    feat: introduce config structure in tests, write helper to obtain config

--- src/telephuzz/constants.py
+++ src/telephuzz/constants.py
@@ -1,4 +1,9 @@
 """File for constants used across various files."""
 
+from pathlib import Path
+
 DOCKER_NETWORK_NAME = "telephuzz_network"
 MITMPROXY = "mitmproxy"
+
+BASE_PATH = Path(__file__).parent.parent.parent
+CONFIG_PATH = BASE_PATH / "config.yaml"
