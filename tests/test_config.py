47d77c2aac82599b169acfc393276c0a1aed770d
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-06-07 16:13:13 +0200

    test: add test that config file is only read once

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,9 @@
 """Tests for config helper methods."""
 
 from pathlib import Path
+from unittest.mock import mock_open, patch
 
+import telephuzz.config as cfg
 from telephuzz.config import Config, get_config
 
 TEST_CONFIG_PATH = Path(__file__).parent / "testfiles" / "config.yaml"
@@ -23,3 +25,24 @@ def test_get_config():
     ]
 
     assert config.timeout == 3600
+
+    cfg._config = None
+
+
+def test_config_singleton():
+    """Test that config file is only read once."""
+    with open(TEST_CONFIG_PATH, "r") as f:
+        config = f.read()
+
+    m = mock_open(read_data=config)
+
+    Config.CONFIG_PATH = TEST_CONFIG_PATH
+
+    with patch("builtins.open", m):
+        cfg1 = cfg.get_config()
+        cfg2 = cfg.get_config()
+
+    assert cfg1 is cfg2
+    assert m.call_count == 1
+
+    cfg._config = None
