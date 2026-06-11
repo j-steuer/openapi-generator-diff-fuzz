2e5c0a6ccd92b779f92ecbc39efdd79d70c28540
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-06-11 14:04:21 +0200

    test: add test for docker compose up and down helpers

--- tests/test_docker_helpers.py
+++ tests/test_docker_helpers.py
@@ -1,11 +1,17 @@
 """Tests for docker helper methods."""
 
+import subprocess
 import tempfile
 from pathlib import Path
 
 from conftest import start_mongodb
 
-from telephuzz.docker_helpers import write_to_container, write_to_host
+from telephuzz.docker_helpers import (
+    compose_down,
+    compose_up,
+    write_to_container,
+    write_to_host,
+)
 from telephuzz.session.api import APIMongoDBDatabaseContainer
 
 
@@ -47,3 +53,29 @@ def test_write_container(mongodb):
 
         exit_code, output = db2.exec_run(f"test -e {path}")
         assert exit_code == 0, output
+
+
+def test_compose():
+    """Test docker compose up and down methods."""
+    compose_file = Path(__file__).parent / "testfiles" / "docker-compose.yml"
+    project = "test_compose"
+
+    compose_up(compose_path=compose_file, project=project)
+
+    assert (
+        project.encode()
+        in subprocess.run(
+            ["docker", "ps", "--filter", f"label=com.docker.compose.project={project}"],
+            capture_output=True,
+        ).stdout
+    )
+
+    compose_down(compose_path=compose_file, project=project)
+
+    assert (
+        project.encode()
+        not in subprocess.run(
+            ["docker", "ps", "--filter", f"label=com.docker.compose.project={project}"],
+            capture_output=True,
+        ).stdout
+    )
