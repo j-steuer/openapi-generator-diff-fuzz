1258b590796a5bfaf639529b51e7a28bb127bc2a
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-06-16 13:58:45 +0200

    feat: implement batches mode for fuzzer based generators

--- tests/test_request_generator.py
+++ tests/test_request_generator.py
@@ -10,42 +10,57 @@ OAS_PATH = Path(__file__).resolve().parent / "testfiles" / "openapi.json"
 
 def test_simple_schemathesis_generator(api):
     """Run SchemathesisGenerator for 3 seconds and perform basic request checks."""
-    generator = SchemathesisGenerator(
+    with SchemathesisGenerator(
         OAS_PATH, "http://localhost:8000", max_time_seconds=3
-    )
+    ) as generator:
+        assert len(generator.pregenerated_requests) >= 1, "No requests were generated."
 
-    assert len(generator.pregenerated_requests) >= 1, "No requests were generated."
+        # run basic Request structure assertions
+        for idx, request in enumerate(generator.pregenerated_requests):
+            print(f"Request {idx}: {repr(request)}")
 
-    # run basic Request structure assertions
-    for idx, request in enumerate(generator.pregenerated_requests):
-        print(f"Request {idx}: {repr(request)}")
+            # method should be GET or POST
+            assert request.method.value in ["GET", "POST"], request.method.value
 
-        # method should be GET or POST
-        assert request.method.value in ["GET", "POST"], request.method.value
+            # path should be /greet or /user
+            assert "/greet" in request.path or "/user" in request.path
 
-        # path should be /greet or /user
-        assert "/greet" in request.path or "/user" in request.path
-
-        # query parameters should match in path and query_parameters
-        if "?" in request.path:
-            assert request.query_parameters, (
-                f"Query parameters in path {request.path}, but empty query parameters"
-            )
+            # query parameters should match in path and query_parameters
+            if "?" in request.path:
+                assert request.query_parameters, (
+                    f"Query parameters in path {request.path}, but empty field."
+                )
 
 
 def test_method_name_schemathesis_generator(api):
     """Test always being able to infer a method name from Schemathesis generator."""
-    generator = SchemathesisGenerator(
+    with SchemathesisGenerator(
         OAS_PATH, "http://localhost:8000", max_time_seconds=3
-    )
+    ) as generator:
+        assert len(generator.pregenerated_requests) >= 1, "No requests were generated."
 
-    assert len(generator.pregenerated_requests) >= 1, "No requests were generated."
+        method_names = [
+            generate_operation_id("GET", "/greet"),
+            generate_operation_id("GET", "/user"),
+            generate_operation_id("POST", "/user"),
+        ]
+        for request in generator.pregenerated_requests:
+            operation_id = generate_operation_id(request.method.value, request.path)
+            assert operation_id in method_names
 
-    method_names = [
-        generate_operation_id("GET", "/greet"),
-        generate_operation_id("GET", "/user"),
-        generate_operation_id("POST", "/user"),
-    ]
-    for request in generator.pregenerated_requests:
-        operation_id = generate_operation_id(request.method.value, request.path)
-        assert operation_id in method_names
+
+def test_batches(api):
+    """Test generating requests in batches."""
+    with SchemathesisGenerator(
+        OAS_PATH, "http://localhost:8000", max_time_seconds=10, batch_time=7
+    ) as generator:
+        # should collect two times
+        first_batch = generator.generate()
+        second_batch = generator.generate()
+        third_batch = generator.generate()
+
+        assert first_batch is not None
+        assert second_batch is not None
+        assert third_batch is None
+
+        assert 0 < len(second_batch) < len(first_batch)
