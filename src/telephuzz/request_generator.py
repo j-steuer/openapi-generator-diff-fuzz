545bf8d5bd2f8634dda305a70dd0786a2b1c5211
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-06-18 14:09:43 +0200

    Revert "feat: implement batches mode for fuzzer based generators"
    This reverts commit 1258b590796a5bfaf639529b51e7a28bb127bc2a.

--- src/telephuzz/request_generator.py
+++ src/telephuzz/request_generator.py
@@ -2,14 +2,11 @@
 
 import logging
 import os
-import signal
 import subprocess
 import sys
 import tempfile
 from abc import ABC, abstractmethod
-from contextlib import ExitStack
 from pathlib import Path
-from time import sleep
 
 from telephuzz.constants import BASE_PATH
 from telephuzz.http_message import Request, Response
@@ -50,8 +47,7 @@ class FuzzerBasedGenerator(OASRequestGenerator):
         oas: Path,
         base_api_url: str,
         cmd: list[str],
-        proxy_port: int = 8081,
-        batch_time: int | None = None,
+        proxy_port: int = 8080,
         log_fuzzer: bool = False,
     ):
         """Pre-generate the requests using mitmproxy.
@@ -63,121 +59,52 @@ class FuzzerBasedGenerator(OASRequestGenerator):
                  of the API, the fuzzer should target "http://localhost:{proxy_port}"
                  WARNING: The provided cmd will be executed without sanitization.
                  Only provide trusted input.
-            proxy_port: The port the mitmproxy instance (defaults to 8081)
-            batch_time: If provided, instead of pregenerating all responses,
-            pause and resume the process as needed for batch_time seconds.
+            proxy_port: The port the mitmproxy instance (defaults to 8080)
             log_fuzzer: If true, display stdout and stderr messages produced by cmd
 
         """
         logger.info("Setting up fuzzer-based generator.")
         self.oas = oas
         self.pregenerated_requests: list[Request] = []
-        self.batch_time = batch_time
 
-        self.exit_stack = ExitStack()
-        self.tmpdir = self.exit_stack.enter_context(tempfile.TemporaryDirectory())
-        self.mitm_proxy = self.exit_stack.enter_context(
-            MITMProxyContainer(
-                response_output=self.tmpdir,
-                listen_port=proxy_port,
-                target=base_api_url,
-            )
-        )
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with MITMProxyContainer(
+                response_output=tmpdir, listen_port=proxy_port, target=base_api_url
+            ) as _:
+                subprocess.run(
+                    cmd,
+                    stdout=sys.stdout if log_fuzzer else subprocess.DEVNULL,
+                    stderr=sys.stderr if log_fuzzer else subprocess.DEVNULL,
+                )
 
-        stdout = sys.stdout if log_fuzzer else subprocess.DEVNULL
-        stderr = sys.stderr if log_fuzzer else subprocess.DEVNULL
-        if self.batch_time is None:
-            subprocess.run(
-                cmd,
-                stdout=stdout,
-                stderr=stderr,
-            )
-        else:
-            self.fuzzer_process = subprocess.Popen(cmd, stdout=stdout, stderr=stderr)
-            sleep(self.batch_time)
-            self._pause_fuzzer()
-            self.first_collection = True
+            logger.info("Finished generating requests.")
 
-        logger.info("Finished generating requests.")
-
-        self._collect_responses(Path(self.tmpdir))
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc, tb):
-        self.exit_stack.close()
-
-    def _pause_fuzzer(self) -> None:
-        """Pause the fuzzing process if fuzzer runs in batch mode."""
-        if not self.batch_time:
-            raise ValueError("Can only pause fuzzer if batch time provided.")
-
-        pid = self.fuzzer_process.pid
-        os.kill(pid, signal.SIGSTOP)
-
-    def _resume_fuzzer(self) -> None:
-        """Resume the fuzzing process if fuzzer runs in batch mode."""
-        if not self.batch_time:
-            raise ValueError("Can only resume fuzzer if batch time provided.")
-
-        pid = self.fuzzer_process.pid
-        os.kill(pid, signal.SIGCONT)
-
-    def _collect_responses(self, dir_path: Path) -> None:
-        """Collect and parse responses."""
-        responses = os.listdir(dir_path)
-        if len(responses) < 1:
-            if not self.batch_time:
+            responses = os.listdir(tmpdir)
+            if len(responses) < 1:
                 raise ValueError(
                     "No responses were captured. "
                     "Your fuzzer needs to produce at least one request."
                 )
-            else:
-                self.pregenerated_requests = list()
 
-        base_response_path = Path(dir_path)
-        for response in responses:
-            response_path = base_response_path / response
-            request_obj = Request.from_json(response_path)
+            base_response_path = Path(tmpdir)
+            for response in responses:
+                response_path = base_response_path / response
+                request_obj = Request.from_json(response_path)
 
-            self.pregenerated_requests.append(request_obj)
+                self.pregenerated_requests.append(request_obj)
 
     def generate(
         self, previous_responses: list[Response] | None = None
     ) -> list[Request] | None:
         """Return pregenerated requests in captured order until empty."""
-        if self.batch_time is None:
-            if not self.pregenerated_requests:
-                return None
+        if not self.pregenerated_requests:
+            return None
 
-            if not hasattr(self, "total"):
-                self.total = len(self.pregenerated_requests)
+        if not hasattr(self, "total"):
+            self.total = len(self.pregenerated_requests)
 
-            request = self.pregenerated_requests.pop(0)
-            return [request]
-
-        else:
-            if self.first_collection:
-                self.first_collection = False
-                return self.pregenerated_requests
-
-            # clear previous requests
-            for response_file in os.listdir(self.tmpdir):
-                os.remove(os.path.join(self.tmpdir, response_file))
-            self.pregenerated_requests = list()
-
-            # collect more requests
-            self._resume_fuzzer()
-            sleep(self.batch_time)
-            self._pause_fuzzer()
-
-            self._collect_responses(Path(self.tmpdir))
-
-            if not self.pregenerated_requests:
-                return None
-
-            return self.pregenerated_requests
+        request = self.pregenerated_requests.pop(0)
+        return [request]
 
 
 class SchemathesisGenerator(FuzzerBasedGenerator):
@@ -193,7 +120,6 @@ class SchemathesisGenerator(FuzzerBasedGenerator):
         base_api_url: str,
         proxy_port: int = 8080,
         max_time_seconds: int = 3600,
-        batch_time: int | None = None,
         log_fuzzer: bool = False,
     ):
         """Initialize the SchemathesisGenerator."""
@@ -217,5 +143,4 @@ class SchemathesisGenerator(FuzzerBasedGenerator):
             cmd=cmd,
             proxy_port=proxy_port,
             log_fuzzer=log_fuzzer,
-            batch_time=batch_time,
         )
--- tests/test_request_generator.py
+++ tests/test_request_generator.py
@@ -10,57 +10,42 @@ OAS_PATH = Path(__file__).resolve().parent / "testfiles" / "openapi.json"
 
 def test_simple_schemathesis_generator(api):
     """Run SchemathesisGenerator for 3 seconds and perform basic request checks."""
-    with SchemathesisGenerator(
+    generator = SchemathesisGenerator(
         OAS_PATH, "http://localhost:8000", max_time_seconds=3
-    ) as generator:
-        assert len(generator.pregenerated_requests) >= 1, "No requests were generated."
+    )
 
-        # run basic Request structure assertions
-        for idx, request in enumerate(generator.pregenerated_requests):
-            print(f"Request {idx}: {repr(request)}")
+    assert len(generator.pregenerated_requests) >= 1, "No requests were generated."
 
-            # method should be GET or POST
-            assert request.method.value in ["GET", "POST"], request.method.value
+    # run basic Request structure assertions
+    for idx, request in enumerate(generator.pregenerated_requests):
+        print(f"Request {idx}: {repr(request)}")
 
-            # path should be /greet or /user
-            assert "/greet" in request.path or "/user" in request.path
+        # method should be GET or POST
+        assert request.method.value in ["GET", "POST"], request.method.value
 
-            # query parameters should match in path and query_parameters
-            if "?" in request.path:
-                assert request.query_parameters, (
-                    f"Query parameters in path {request.path}, but empty field."
-                )
+        # path should be /greet or /user
+        assert "/greet" in request.path or "/user" in request.path
+
+        # query parameters should match in path and query_parameters
+        if "?" in request.path:
+            assert request.query_parameters, (
+                f"Query parameters in path {request.path}, but empty query parameters"
+            )
 
 
 def test_method_name_schemathesis_generator(api):
     """Test always being able to infer a method name from Schemathesis generator."""
-    with SchemathesisGenerator(
+    generator = SchemathesisGenerator(
         OAS_PATH, "http://localhost:8000", max_time_seconds=3
-    ) as generator:
-        assert len(generator.pregenerated_requests) >= 1, "No requests were generated."
+    )
 
-        method_names = [
-            generate_operation_id("GET", "/greet"),
-            generate_operation_id("GET", "/user"),
-            generate_operation_id("POST", "/user"),
-        ]
-        for request in generator.pregenerated_requests:
-            operation_id = generate_operation_id(request.method.value, request.path)
-            assert operation_id in method_names
+    assert len(generator.pregenerated_requests) >= 1, "No requests were generated."
 
-
-def test_batches(api):
-    """Test generating requests in batches."""
-    with SchemathesisGenerator(
-        OAS_PATH, "http://localhost:8000", max_time_seconds=10, batch_time=7
-    ) as generator:
-        # should collect two times
-        first_batch = generator.generate()
-        second_batch = generator.generate()
-        third_batch = generator.generate()
-
-        assert first_batch is not None
-        assert second_batch is not None
-        assert third_batch is None
-
-        assert 0 < len(second_batch) < len(first_batch)
+    method_names = [
+        generate_operation_id("GET", "/greet"),
+        generate_operation_id("GET", "/user"),
+        generate_operation_id("POST", "/user"),
+    ]
+    for request in generator.pregenerated_requests:
+        operation_id = generate_operation_id(request.method.value, request.path)
+        assert operation_id in method_names
