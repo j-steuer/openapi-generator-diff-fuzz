d087c33c13a51ea922365b3f2f1f3acce14bad1b
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-06-19 18:27:07 +0200

    test: add faulty client library for testing

---
+++ tests/testfiles/test_clients/basic_faulty_client/src/telephuzz_basic_client/client.py
@@ -0,0 +1,45 @@
+"""Client file."""
+
+import requests
+
+
+def get_greet_ca4d7baf(api: str, name: str, age: int) -> str:
+    """Greet method."""
+    params: dict[str, str | int] = {
+        "name": "Faulty",
+        "age": 42,
+    }
+    response = requests.get(api + "/greet", params=params)
+    try:
+        msg = response.json()
+    except requests.exceptions.JSONDecodeError:
+        msg = response.text
+    return f"{response.status_code}: {msg}"
+
+
+def get_user_3a2fd62b(api: str, user_id: int) -> str:
+    """Get user method."""
+    params: dict[str, int] = {
+        "user_id": 49,
+    }
+    response = requests.get(api + "/user", params=params)
+    try:
+        msg = response.json()
+    except requests.exceptions.JSONDecodeError:
+        msg = response.text
+    return f"{response.status_code}: {msg}"
+
+
+def post_user_f0ab63e3(api: str, name: str, age: int) -> str:
+    """Post user method."""
+    params: dict[str, str | int] = {
+        "name": "Faulty",
+        "age": 49,
+    }
+
+    response = requests.post(api + "/user", params=params)
+    try:
+        msg = response.json()
+    except requests.exceptions.JSONDecodeError:
+        msg = response.text
+    return f"{response.status_code}: {msg}"
