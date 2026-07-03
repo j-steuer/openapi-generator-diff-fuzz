914e72ddc31dfe86a72d398a914db91ea8bb791a
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-07-03 15:01:12 +0200

    fix: remove debug log in mitmproxy request hook

--- src/telephuzz/session/mitm_proxy/proxy_hooks.py
+++ src/telephuzz/session/mitm_proxy/proxy_hooks.py
@@ -13,12 +13,6 @@ RESPONSE_PATH = "/responses"
 
 def request(flow: http.HTTPFlow):
     """Handle custom encoding to route requests to API targets."""
-    log = "/tmp/logs"
-    os.makedirs(log, exist_ok=True)
-    if len(os.listdir(log)) < 3:
-        with open(f"{log}/log{time.time_ns()}.txt", "w") as f:
-            f.write(repr(flow))
-
     path = flow.request.path  # e.g. /server:8000/api/foo
 
     parts = path.lstrip("/").split("/", 1)
