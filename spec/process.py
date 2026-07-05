d101aa7f8801f6c95bec7d052c8a4781153dad01
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-07-05 18:23:44 +0200

    feat: remove process.py

--- spec/process.py
+++
@@ -1,6 +0,0 @@
-from telephuzz.constants import BASE_PATH
-from telephuzz.openapi_helpers import preprocess_oas
-
-if __name__ == "__main__":
-    path = BASE_PATH / "spec" / "openapi.json"
-    preprocess_oas(path, path)
