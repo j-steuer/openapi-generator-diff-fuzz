cedeea030b877e8c4e770ac9bf43cda2672b9acd
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-07-18 14:15:12 +0200

    fix: fix output path of openapi-python-generator script

--- generators/openapi-python-generator.sh
+++ generators/openapi-python-generator.sh
@@ -6,10 +6,15 @@ if [ $# -ne 1 ]; then
   exit 1
 fi
 
-OUTPUT_PATH="$(realpath "$1")"
+OUTPUT_PATH="$(realpath -m "$1")"
 PROJECT_ROOT="$(pwd)"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+PARENT_DIR="$(dirname "$OUTPUT_PATH")"
+OUT_NAME="$(basename "$OUTPUT_PATH")"
+
+mkdir -p "$PARENT_DIR"
 
 docker build \
   -t telephuzz:openapi-python-client \
@@ -19,9 +24,9 @@ docker build \
 docker run --rm \
   --user "$(id -u):$(id -g)" \
   -v "$PROJECT_ROOT:/local" \
-  -v "$OUTPUT_PATH:/local/output" \
+  -v "$PARENT_DIR:/output" \
   telephuzz:openapi-python-client \
   generate \
   --path /local/spec/openapi.json \
-  --output-path /local/output \
+  --output-path "/output/$OUT_NAME" \
   --overwrite
