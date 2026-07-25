e8e96f509b5140cf770cec7b2b58c9996f6a9b1d
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-07-25 15:09:46 +0200

    test: add petshop test with all python clients

--- tests/test_loop.py
+++ tests/test_loop.py
@@ -410,3 +410,11 @@ def test_loop_petshop(client_class: type, client_generator):
         fuzzer.start_fuzzing_session()
 
         assert len(os.listdir(LOG_PATH)) == 0
+
+
+def test_python_petshop() -> None:
+    Config.API_CONFIG_PATH = TEST_CONFIG_BASE_PATH / "api_petshop_config.yaml"
+    Config.CLIENT_CONFIG_PATH = TEST_CONFIG_BASE_PATH / "client_python_config.yaml"
+
+    fuzzer = TelePhuzz()
+    fuzzer.start_fuzzing_session()
