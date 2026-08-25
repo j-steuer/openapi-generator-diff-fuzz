7f748eed1ae3fdaa01fa2223f340caa23764e290
Justin Steuer <s8jnsteu@stud.uni-saarland.de>
2026-08-25 19:35:41 +0200

    test: add failing test in test_session highlighting trailing dot issue in OpenAPI generator

---
+++ tests/test_session.py
@@ -0,0 +1,36 @@
+import pytest
+from conftest import TEST_CONFIG_BASE_PATH
+from requests.models import CaseInsensitiveDict
+
+from telephuzz.config import Config
+from telephuzz.http_message import HTTPMethod, Request
+from telephuzz.session.session import SessionManager
+
+
+@pytest.mark.xfail(
+    reason=(
+        "Fails due to OpenAPI Generator Python using urllib3, "
+        "which strips trailing dots from the URL"
+    )
+)
+def test_dot_path_variable():
+    """Test correct translation of dot path variable."""
+    Config.API_CONFIG_PATH = TEST_CONFIG_BASE_PATH / "api_petshop_config.yaml"
+    Config.CLIENT_CONFIG_PATH = TEST_CONFIG_BASE_PATH / "client_python_config.yaml"
+
+    request = Request(
+        headers=CaseInsensitiveDict(),
+        body=b"",
+        method=HTTPMethod.GET,
+        path="/user/.",
+        query_parameters={},
+    )
+
+    with SessionManager() as session_manager:
+        session_manager.send(request)
+        results = session_manager.send(request)
+
+        result = next(iter(results))
+
+        assert result.request
+        assert result.request.path == request.path
